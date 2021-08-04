9dc936de5732392682b95a0332da30b85f8afdbe
Santio <santiothebest123@gmail.com>
2021-08-04 15:25:43 -0400

    Finish tablist and nametag handling

--- src/main/java/me/santio/site39/listeners/TablistListener.java
+++ src/main/java/me/santio/site39/listeners/TablistListener.java
@@ -6,6 +6,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.ScoreboardManager;
 import org.bukkit.scoreboard.Team;
@@ -21,16 +22,32 @@ public class TablistListener implements Listener {
         if (scoreboardManager == null) return;
         
         String rank = RankUtils.getRank(event.getPlayer().getUniqueId());
-        int weightR = 10000 - RankUtils.getWeight(rank);
+        int weightR = 9999 - RankUtils.getWeight(rank);
+        String teamID = weightR + "-" + event.getPlayer().getUniqueId().toString().substring(0, 10);
     
         Scoreboard scoreboard = scoreboardManager.getMainScoreboard();
-        Team team = scoreboard.getTeam(weightR + "-" + rank);
-        if (team == null) team = scoreboard.registerNewTeam(weightR + "-" + rank);
+        Team team = scoreboard.getTeam(teamID);
+        if (team == null) team = scoreboard.registerNewTeam(teamID);
         
-        team.setPrefix(TextUtils.tacc(RankUtils.getPrefix(rank) + "&8 | &7"));
+        team.setPrefix(TextUtils.tacc(RankUtils.getPrefix(event.getPlayer().getUniqueId()) + "&8 | &7"));
         event.getPlayer().setPlayerListName(TextUtils.tacc(RankUtils.getColor(event.getPlayer().getUniqueId())+event.getPlayer().getName()));
         
         team.addPlayer(event.getPlayer());
+    }
+    
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event) {
+        if (scoreboardManager == null) return;
+        
+        String rank = RankUtils.getRank(event.getPlayer().getUniqueId());
+        int weightR = 9999 - RankUtils.getWeight(rank);
+        String teamID = weightR + "-" + event.getPlayer().getUniqueId().toString().substring(0, 10);
+        
+        Team team = scoreboardManager.getMainScoreboard().getTeam(teamID);
+        if (team == null) return;
+        
+        team.removePlayer(event.getPlayer());
+        team.unregister();
     }
     
 }
